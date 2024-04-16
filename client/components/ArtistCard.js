7107514c4b475bbcc94849ad24f8db26056c1f7b
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-04-16 11:54:44 +0300

    feat: handle artist card info

---
+++ client/components/ArtistCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../stylesheets/ArtistCard.scss';
+
+const ArtistCard = ({ artist, onSetArtist }) => {
+  const { name, image_url, id } = artist;
+
+  function handleClick() {
+    onSetArtist(id)
+  }
+
+  return (
+    <div onClick={handleClick} className="artist-card">
+        <div className="image">
+          <img src={image_url} alt={name}/>
+        </div>
+        <div className="name">
+            {name}
+        </div>
+    </div>
+  )
+}
+
+export default ArtistCard;

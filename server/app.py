81f9c42cea14c6ac71e7714268366a1e53294d07
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-04-16 18:09:11 +0300

    feat:set POST method to add a new music artist

--- server/app.py
+++ server/app.py
@@ -20,4 +20,11 @@ class Music(Resource):
     def get(self):
         music = [artist.to_dict() for artist in Artist.query.all()]
         return make_response(music, 200) 
-       
+    
+    def post(self):
+        request_json = request.get_json()
+
+        name = request_json.get('name')
+        spotify_id= request_json.get('spotifyId')
+        image_url = request_json.get('imageUrl')
+        genres = request_json.get('genres')       

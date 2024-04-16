bbefec67df888af09dcd27c8923394eb5920d415
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-04-16 19:54:29 +0300

    feat:Set POST method for user login

--- server/app.py
+++ server/app.py
@@ -156,3 +156,23 @@ class CheckSession(Resource):
             user = User.query.filter(User.id == session['user_id']).first()
             return user.to_dict(), 200
         return {'error': '401 Unauthorized'}, 401        
+    
+class Login(Resource):
+    def post(self):
+
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            if user.authenticate(password):
+
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+            return {'error': 'Incorrect password.'}, 401
+
+        return {'error': '401 Unauthorized'}, 401    
+    

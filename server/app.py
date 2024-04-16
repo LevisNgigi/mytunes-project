cb61214441504283bbb9aadec28d9283b26b9665
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-04-16 17:27:55 +0300

    feat:add GET method to fetch all users

---
+++ server/app.py
@@ -0,0 +1,17 @@
+from flask import request, session, make_response
+from flask_restful import Api, Resource
+from sqlalchemy.exc import IntegrityError
+
+from config import app, db
+from models import User, Artist, Playlist, Song
+
+api = Api(app)
+
+@app.route('/')
+def index():
+    return '<h1>myTunes Back End Development</h1>'
+
+class Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(users, 200)  
